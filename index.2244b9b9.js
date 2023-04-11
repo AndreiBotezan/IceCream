e4d58c12b4180c82c69b4acca92e64def8ebd6ee
AndreiBotezan <73821849+AndreiBotezan@users.noreply.github.com>
2023-04-11 14:33:37 +0000

    Deploying to gh-pages from @ AndreiBotezan/IceCream@8c541e248902a21cd147db7a77a29e47aece6543 🚀

---
+++ index.2244b9b9.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.2244b9b9.js.map
