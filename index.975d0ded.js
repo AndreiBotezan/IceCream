4002a6d039f7fa4a9ee58583305e2a624a21a5f1
AndreiBotezan <73821849+AndreiBotezan@users.noreply.github.com>
2023-04-12 06:39:05 +0000

    Deploying to gh-pages from @ AndreiBotezan/IceCream@f5fec785b2b7d3115467de58bcce76e75e2d99d9 🚀

---
+++ index.975d0ded.js
@@ -0,0 +1,2 @@
+function myFunction(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("myButton");"none"===e.style.display?(e.style.display="inline",t.innerHTML="Read more &gt;",n.style.display="none"):(e.style.display="none",t.innerHTML="Read less &lt;",n.style.display="inline")}
+//# sourceMappingURL=index.975d0ded.js.map
